4a99f3e491e0e0828d5eb5cf2ede65dcbbd857e7
Ivan Pekov <ivan@mrivanplays.com>
2020-09-23 18:17:18 +0300

    Updated Upstream (Paper)
    Upstream has released updates that appears to apply and compile correctly
    
    Paper Changes:
    079a793b Revert "Only consider chunks Loaded if at BORDER status"
    eda80e3d Prevent fire from spreading to invalid locations

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -62,7 +62,7 @@ function applyPatch {
     git reset --hard upstream/upstream
     echo "  Applying patches to $target..."
     git am --abort >/dev/null 2>&1
-    git am --3way --ignore-whitespace "$basedir/patches/$patch_folder/"*.patch
+    git am --reject --3way --ignore-whitespace "$basedir/patches/$patch_folder/"*.patch
     if [ "$?" != "0" ]; then
         echo "  Something did not apply cleanly to $target."
         echo "  Please review above details and finish the apply then"
